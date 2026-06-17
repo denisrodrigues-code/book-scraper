8bda79d68a71a24f836d26e018015399d4159188
Denis Rodrigues <denisrodrigues.dev@gmail.com>
2026-06-17 15:07:16 -0300

    feat: add concurrent page scraping with worker pool

--- internal/scraper/crawler.go
+++ internal/scraper/crawler.go
@@ -2,39 +2,89 @@ package scraper
 
 import (
 	"fmt"
+	"sync"
 	"time"
 
 	"book-scraper/internal/models"
 )
 
+const (
+	totalPages   = 50
+	workerCount  = 5
+	requestDelay = 300 * time.Millisecond
+)
+
 func ScrapeBooks() ([]models.Book, error) {
+	jobs := make(chan string)
+	results := make(chan []models.Book)
+	errors := make(chan error, 1)
+
+	var wg sync.WaitGroup
+
+	limiter := time.NewTicker(requestDelay)
+	defer limiter.Stop()
+
+	for i := 0; i < workerCount; i++ {
+		wg.Add(1)
+
+		go func() {
+			defer wg.Done()
+
+			for url := range jobs {
+				<-limiter.C
+
+				books, err := ScrapePage(url)
+				if err != nil {
+					select {
+					case errors <- err:
+					default:
+					}
+					return
+				}
+
+				results <- books
+			}
+		}()
+	}
+
+	go func() {
+		defer close(jobs)
+
+		for page := 1; page <= totalPages; page++ {
+			jobs <- buildPageURL(page)
+		}
+	}()
+
+	go func() {
+		wg.Wait()
+		close(results)
+	}()
 
 	var allBooks []models.Book
 
-	for page := 1; page <= 50; page++ {
-
-		var url string
-
-		if page == 1 {
-			url = baseURL
-		} else {
-			url = fmt.Sprintf(
-				"%scatalogue/page-%d.html",
-				baseURL,
-				page,
-			)
-		}
-
-		books, err := ScrapePage(url)
-
-		if err != nil {
+	for {
+		select {
+		case err := <-errors:
 			return nil, err
+
+		case books, ok := <-results:
+			if !ok {
+				return allBooks, nil
+			}
+
+			allBooks = append(allBooks, books...)
 		}
+	}
+}
 
-		allBooks = append(allBooks, books...)
-
-		time.Sleep(1 * time.Second)
+func buildPageURL(page int) string {
+	if page == 1 {
+		return baseURL
 	}
 
-	return allBooks, nil
+	return fmt.Sprintf(
+		"%scatalogue/page-%d.html",
+		baseURL,
+		page,
+	)
 }
